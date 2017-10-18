ce7e67d32d98bb7cede66254aa9a7f741c8f0dda
Jacques Bellavance <j-bellavance@videotron.ca>
2017-10-18 16:24:39 -0400

    Add files via upload

---
+++ State machines Tutorial/Part 2/TrafficLights/TrafficLights.ino
@@ -0,0 +1,147 @@
+/*
+ TrafficLights.ino
+ Author: Jacques Bellavance
+ Released: October 18, 2017
+           under GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 b
+
+ This code is to demonstrate the use of state machines in an Arduino sketch.
+ It is fully explained in the tutorial: State machine with Arduino (Part 2)
+*/
+//The switch for pedestrians===============================================================
+#include <EdgeDebounceLite.h>
+EdgeDebounceLite debounce;
+
+bool pedestrians = false;
+byte switchPin = 10;
+
+enum SwitchStates {IS_OPEN, IS_RISING, IS_CLOSED, IS_FALLING};
+SwitchStates switchState = IS_OPEN;
+
+enum SwitchModes {PULLUP, PULLDOWN};
+SwitchModes switchMode = PULLUP;
+
+void readSwitch() {
+  byte pinIs = debounce.pin(switchPin);
+  if (switchMode == PULLUP) pinIs = !pinIs;
+  switch (switchState) {
+    case IS_OPEN:    { if(pinIs == HIGH)   switchState = IS_RISING;  break; }
+    case IS_RISING:  {                     switchState = IS_CLOSED;  break; }
+    case IS_CLOSED:  { if(pinIs == LOW)    switchState = IS_FALLING; break; }
+    case IS_FALLING: { pedestrians = true; switchState = IS_OPEN;    break; }
+  }
+}
+
+//The LEDs===============================================================================
+#define MAIN_G_PIN 2    //Main Street green LED pin
+#define MAIN_Y_PIN 3    //Main Street yellow LED pin
+#define MAIN_R_PIN 4    //Main Street red LED pin
+#define PINE_G_PIN 5    //Pine Street green LED pin
+#define PINE_Y_PIN 6    //Pine Street yellow LED pin
+#define PINE_R_PIN 7    //Pine Street red LED pin
+#define TURN_PIN   8    //Turn left LED pin
+#define WALK_PIN   9    //Pedestrian LED pin
+
+#define BLINK_SPEED 250
+unsigned long blinkChrono;
+
+void blink(byte ID) {
+  if (millis() - blinkChrono >= BLINK_SPEED) {
+    blinkChrono = millis();
+    digitalWrite(ID, !digitalRead(ID));
+  }
+}
+
+//The traffic lights state machine========================================================
+enum TrafficLights { MAIN_G, MAIN_Y, MAIN_R, PINE_G, PINE_Y, PINE_R,
+                     TURN_G, TURN_Y, TURN_R, WALK_G, WALK_Y, WALK_R};
+TrafficLights statusTL = WALK_R;
+
+unsigned long chrono;
+
+void trafficLights() {
+  switch(statusTL) {
+    case MAIN_G: { if (millis() - chrono >= 20000) { 
+                     chrono = millis(); 
+                     digitalWrite(MAIN_G_PIN,  LOW);
+                     digitalWrite(MAIN_Y_PIN, HIGH); 
+                     statusTL = MAIN_Y; } 
+                   break; }
+    case MAIN_Y: { if (millis() - chrono >=  3000) {
+                     chrono = millis(); 
+                     digitalWrite(MAIN_Y_PIN, LOW);
+                     digitalWrite(MAIN_R_PIN, HIGH); 
+                     statusTL = MAIN_R; } 
+                   break; }
+    case MAIN_R: { if (millis() - chrono >=  2000) {
+                     chrono = millis(); 
+                     digitalWrite(PINE_R_PIN,  LOW);
+                     digitalWrite(PINE_G_PIN, HIGH); 
+                     statusTL = PINE_G; } 
+                   break; }
+    case PINE_G: { if (millis() - chrono >= 10000) {
+                     chrono = millis(); 
+                     digitalWrite(PINE_G_PIN,  LOW);
+                     digitalWrite(PINE_Y_PIN, HIGH); 
+                     statusTL = PINE_Y; } 
+                   break; }
+    case PINE_Y: { if (millis() - chrono >=  3000) {
+                      chrono = millis(); 
+                      digitalWrite(PINE_Y_PIN, LOW);
+                      digitalWrite(PINE_R_PIN, HIGH);
+                      statusTL = PINE_R; } 
+                   break; }
+    case PINE_R: { if (millis() - chrono >=  2000) {
+                     chrono = millis();
+                     digitalWrite(TURN_PIN, HIGH);
+                     statusTL = TURN_G; } 
+                   break; }
+    case TURN_G: { if (millis() - chrono >= 10000) { 
+                     chrono = millis(); 
+                     statusTL = TURN_Y; } 
+                   break; }
+    case TURN_Y: { blink(TURN_PIN);
+                   if (millis() - chrono >=  3000) {
+                     chrono = millis();
+                     digitalWrite(TURN_PIN, LOW); 
+                     statusTL = TURN_R; } 
+                   break; }
+    case TURN_R: { if (millis() - chrono >=  2000) {
+                     chrono = millis();
+                     if (pedestrians) { 
+                        pedestrians = false;
+                        digitalWrite(WALK_PIN, HIGH);
+                        statusTL = WALK_G; }
+                     else { 
+                       digitalWrite(MAIN_R_PIN,  LOW); digitalWrite(MAIN_G_PIN, HIGH);
+                       statusTL = MAIN_G; } }
+                   break; }
+    case WALK_G: { if (millis() - chrono >=  5000) {
+                      chrono = millis();
+                      statusTL = WALK_Y; } 
+                    break; }
+    case WALK_Y:  { blink(WALK_PIN); 
+                    if (millis() - chrono >= 10000) {
+                      chrono = millis(); 
+                      digitalWrite(WALK_PIN, LOW);
+                      statusTL = WALK_R; }
+                     break; }
+    case WALK_R:  { if (millis() - chrono >=  2000) {
+                      chrono = millis(); 
+                      digitalWrite(MAIN_R_PIN,  LOW); digitalWrite(MAIN_G_PIN, HIGH); 
+                      statusTL = MAIN_G; } 
+                   break; }
+  }
+}
+
+void setup() {
+  for (byte i =2 ; i <= 9 ; i++) { pinMode(i, OUTPUT); digitalWrite(i, LOW); }
+  pinMode(switchPin, INPUT_PULLUP);
+  digitalWrite(MAIN_R_PIN, HIGH);
+  digitalWrite(PINE_R_PIN, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  readSwitch();
+  trafficLights();
+}
---
+++ State machines Tutorial/Part 2/VendingMachine/VendingMachine.ino
@@ -0,0 +1,79 @@
+/*
+ VendingMachine.ino
+ Author: Jacques Bellavance
+ Released: October 13, 2017
+           under GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 b
+
+ This code is to demonstrate the use of state machines in an Arduino sketch.
+ It is fully explained in the tutorial: State machine with Arduino (Part 2)
+*/
+
+#include <EdgeDebounceLite.h>
+EdgeDebounceLite debounce;
+
+enum VendingMachineStates {WAIT, TWENTYFIVE, FIFTY, GIVE_CHANGE, DROP_CAN};
+VendingMachineStates vmState = WAIT;
+
+enum SwitchStates {IS_OPEN, IS_RISING, IS_CLOSED, IS_FALLING};
+SwitchStates switchState[2] = {IS_OPEN, IS_OPEN};
+byte switchPin[2] = {10, 11};
+
+enum SwitchModes {PULLUP, PULLDOWN};
+SwitchModes switchMode[2] = {PULLUP, PULLUP};
+
+byte ledPin[2] = {3, 2};
+
+void switchMachine(byte i) {
+  byte pinIs = debounce.pin(switchPin[i]);
+  if (switchMode[i] == PULLUP) pinIs = !pinIs;
+  switch (switchState[i]) {
+    case IS_OPEN:    { if(pinIs == HIGH) switchState[i] = IS_RISING;  break; }
+    case IS_RISING:  {                   switchState[i] = IS_CLOSED;  break; }
+    case IS_CLOSED:  { if(pinIs == LOW)  switchState[i] = IS_FALLING; break; }
+    case IS_FALLING: {                   switchState[i] = IS_OPEN;    break; }
+  }
+}
+
+bool dollarInserted() {
+  switchMachine(0);
+  if (switchState[0] == IS_FALLING) return true;
+  else                              return false;
+}
+
+bool quarterInserted() {
+  switchMachine(1);
+  if (switchState[1] == IS_FALLING) return true;
+  else                              return false;
+}
+void blinkLed(byte i) {
+  digitalWrite(ledPin[i], HIGH);
+  delay(50);
+  digitalWrite(ledPin[i], LOW);
+}
+
+void giveChange() {
+  blinkLed(0);
+}
+
+void dropCan() {
+  blinkLed(1);
+}
+void vendingMachine() {
+  switch (vmState) {
+    case WAIT:        { if (dollarInserted())  vmState = GIVE_CHANGE;
+                        if (quarterInserted()) vmState = TWENTYFIVE;   break; }
+    case TWENTYFIVE:  { if (quarterInserted()) vmState = FIFTY;        break; }
+    case FIFTY:       { if (quarterInserted()) vmState = DROP_CAN;     break; }
+    case GIVE_CHANGE: { giveChange();          vmState = DROP_CAN;     break; }
+    case DROP_CAN:    { dropCan();             vmState = WAIT;         break; }
+  }
+}
+
+void setup() {
+  for (int i = 0 ; i < 2 ; i++)   pinMode(switchPin[i], INPUT_PULLUP);
+  for (int i = 0 ; i < 2 ; i++) { pinMode(ledPin[i],    OUTPUT)      ; digitalWrite(ledPin[i], LOW); }
+}
+void loop() {
+  vendingMachine();
+}
+
